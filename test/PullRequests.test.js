94714772b782be382bd01a550054fe2583efd268
Robbie Marcelo <rbmrclo@hotmail.com>
2017-08-15 18:47:11 +0800

    Adds test for PullRequests isIgnorable

---
+++ test/PullRequests.test.js
@@ -0,0 +1,16 @@
+const PullRequests = require('../src/PullRequests')
+
+test('.isIgnorable(pr) returns true with matched strings', () => {
+  const pullRequest = new PullRequests()
+  const pullRequests = [
+    { title: "Dont merge - this is a PR title" },
+    { title: "Don't merge - this is a PR title" },
+    { title: "Do not merge - this is a PR title" },
+    { title: "[DO NOT MERGE] - this is a PR title" },
+    { title: "WIP - this is a PR title" }
+  ]
+
+  for (let pr of pullRequests) {
+    expect(pullRequest.isIgnorable(pr)).toBeTruthy()
+  }
+})
